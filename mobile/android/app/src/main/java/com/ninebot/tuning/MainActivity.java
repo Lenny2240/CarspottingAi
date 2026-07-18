28556d3e270d6f1590c20c3a38379fe83df23544
Lenny2240 <maui-katze@gmx.de>
2026-07-18 17:51:04 +0200

    Add MainActivity.java for React Native

---
+++ mobile/android/app/src/main/java/com/ninebot/tuning/MainActivity.java
@@ -0,0 +1,23 @@
+// MainActivity.java - React Native Android Entry Point
+package com.ninebot.tuning;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "NinebotTuning";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricDelegate());
+  }
+}
